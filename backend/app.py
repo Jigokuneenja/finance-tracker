f135b9e857431d5834d737e715b4ad58ee8b1750
Jigokuneenja <isaacbwoods@gmail.com>
2026-04-12 14:16:05 -0500

    Add POST /transactions endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from database import init_db
+from flask import Flask, request, jsonify
+from database import init_db, get_connection
 
 app = Flask(__name__)
 
@@ -7,6 +7,33 @@ app = Flask(__name__)
 @app.route('/home')
 def index():
     return  'Finance Tracker API is running'
+
+@app.route('/transactions', methods=['POST'])
+def add_transaction():
+    data = request.get_json()
+    print('Received data:', data)
+
+    amount = data.get('amount')
+    category = data.get('category')
+    date = data.get('date')
+    note = data.get('note', '')
+
+    print('amount:', amount, type(amount))
+    print('category:', category, type(category))
+    print('date:', date, type(date))
+    if amount is None or not category or not date:
+        return jsonify({'error': 'ammount, category, and date are required'}), 400
+    
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO transactions (amount, category, date, note)
+        VALUES (?, ?, ?, ?)
+    ''', (amount, category, date, note))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Transaction added seccessfully'}), 201
 
 if __name__ == '__main__':
     init_db()
